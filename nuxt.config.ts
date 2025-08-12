a6b0d15f11b35fb02c3514b07d2d64832685a4cc
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-08-12 08:47:18 +0700

    feat: init and setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: '@/components/ui',
+  }
 })
